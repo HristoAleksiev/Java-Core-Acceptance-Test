2e44a4717dad9731d0dddf75981cfc41fb63949e
HristoAleksiev <kadoll555@gmail.com>
2015-02-17 20:51:38 +0200

    clean main

--- alphabetinsubstring/src/alphabetinsubstring/Alphabetinsubstring.java
+++ alphabetinsubstring/src/alphabetinsubstring/Alphabetinsubstring.java
@@ -6,13 +6,7 @@ public class Alphabetinsubstring {
         String str = "abcdefghijklmn124345678!@#$%^&*opqrstuvwxyz!*abcdefghijklmn";
         SmallestSubstring subStr = new SmallestSubstring();
         
-        
         String thesubstr = subStr.smallestSubstringContainingTheAlphabet(str);
-//        subStr.printSubstring(subStr.shortestSubstring);
-        
-//        for (List<Element> element : subStr.substringVariants) {
-//            subStr.printSubstring(element);
-//        }
         
         System.out.println(thesubstr);
     }
