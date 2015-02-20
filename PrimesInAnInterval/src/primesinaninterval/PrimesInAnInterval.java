caa4805e4cc004089f77c12a6ef4c4f098e9f590
HristoAleksiev <kadoll555@gmail.com>
2015-02-20 22:50:25 +0200

    cleaned PrimesInAnInterval

--- PrimesInAnInterval/src/primesinaninterval/PrimesInAnInterval.java
+++ PrimesInAnInterval/src/primesinaninterval/PrimesInAnInterval.java
@@ -6,21 +6,10 @@ public class PrimesInAnInterval {
 
     public static void main(String[] args) throws Exception {
         
-        long startTime = System.nanoTime();
-        
         Primes primes = new Primes();
         List primesList;
         
         primesList = primes.primesInAnInterval(2, 30);
         System.out.println(primesList.toString());
-        
-        
-        long endTime = System.nanoTime();
-        System.out.println(endTime - startTime);
-        
-        System.out.println("");
-        System.out.println(primesList.size());
-        
-        //  System.out.println(primes.toString());
     }
 }
