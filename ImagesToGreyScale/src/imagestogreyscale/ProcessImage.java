f5f9e391effda20bd5aefbe4924f10078fcf22dd
HristoAleksiev <kadoll555@gmail.com>
2015-02-24 21:24:02 +0200

    fix ImagesToGreyScale wroks with: jpg, png, bmp

--- ImagesToGreyScale/src/imagestogreyscale/ProcessImage.java
+++ ImagesToGreyScale/src/imagestogreyscale/ProcessImage.java
@@ -8,46 +8,82 @@ import javax.imageio.ImageIO;
 
 public class ProcessImage {
     private BufferedImage image;
-    private BufferedImage output;
+    private BufferedImage outputImage;
     private File imageAsFile;
-    private final File outputImage = new File("C:\\Downloads\\TestFolder\\output.png");
     private WritableRaster raster;
-    public double[] pixels;
+    private double[] pixels;
+    private PictureType pictureType;
+    private File outputImageFile;
     
     public void convertToGreyscale(String path) throws IOException{
+        readImage(path);
+        changeColors();
+        writeGreyPicture();
+    }
+    
+    private void readImage(String path) throws IOException{
         imageAsFile = new File(path);
         image = ImageIO.read(imageAsFile);    
         raster = image.getRaster();
-        
+        findImageType();
         int width = raster.getWidth();
         int height = raster.getHeight();
         
         //  read into array of pixels 
-        double[] preArray = new double[(width * height) * 4];
+        double[] preArray = new double[(width * height) * pictureType.multyplier];
         pixels = raster.getPixels(0, 0, width, height, preArray);
-        
-        // pixels = raster.getPixel(0, 0, preArray);
-        
-        
-        // change colors
-        for (int i = 2; i < pixels.length; i += 4) {
-            
-            // pixels[i - 3] = 100;
-            pixels[i + 1] = 255 - pixels[i];
-            
-            pixels[i] = 0;
-            pixels[i - 1] = 0;
-            pixels[i - 2] = 0;
+    }
+    
+    private void findImageType(){
+        pictureType = new PictureType();
+        if (raster.getNumBands() == 3) {
+            pictureType.bufferdImageType = 4;
+            pictureType.fileType = "jpg";
+            pictureType.multyplier = 3;
         }
+        else if(raster.getNumBands() == 4){
+            pictureType.bufferdImageType = 2;
+            pictureType.fileType = "png";
+            pictureType.multyplier = 4;
+        }
+    }
+    
+    private void changeColors(){
+        if (pictureType.bufferdImageType == 2) {
+            for (int i = 2; i < pixels.length; i += 4) {
+                if (pixels[i + 1] > 250) {
+                    pixels[i + 1] = 255 - pixels[i];
+                    pixels[i] = 0;
+                    pixels[i - 1] = 0;
+                    pixels[i - 2] = 0;
+                }
+                else{
+                    pixels[i] = 0;
+                    pixels[i - 1] = 0;
+                    pixels[i - 2] = 0;
+                }
+            }
+        }
+        else if(pictureType.bufferdImageType == 4){
+            for (int i = 2; i < pixels.length; i += 3) {
+                double grey = (pixels[i] + pixels[i - 1] + pixels[i - 2]) / 3;
+                pixels[i - 2] = grey;
+                pixels[i - 1] = grey;
+                pixels[i] = grey;
+            }
+        }
+    }
+    
+    private void writeGreyPicture() throws IOException{
+        outputImageFile = new File("C:\\Downloads\\TestFolder\\output." + pictureType.fileType);
         
         //  change pixels in raster (changed colors)
-        raster.setPixels(0, 0, width, height, pixels);
+        raster.setPixels(0, 0, raster.getWidth(), raster.getHeight(), pixels);
         
-        // write output
-        output = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        output.setData(raster);
-        
-        ImageIO.write(output, "png", outputImage);
+        //  write output
+        outputImage = new BufferedImage(raster.getWidth(), raster.getHeight(), pictureType.bufferdImageType);
+        outputImage.setData(raster);
+        ImageIO.write(outputImage, pictureType.fileType, outputImageFile);
     }
     
     public String readPixelsArray(){
